daa108003136f183efb6cf4facc9c973cbaf6d77
Vishal1183-bca <147507684+Vishal1183-bca@users.noreply.github.com>
2025-09-06 22:28:52 +0530

    Create Add_product and Category Page in Admin section

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -15,7 +15,18 @@ public class AdminController
     {
         return "admin/index";
     }
-   
+    
+   @GetMapping("/loadAddProduct")
+    public String loadAddProduct() 
+    {
+        return "admin/add_product";
+    }
+       @GetMapping("/category")
+    public String category() 
+    {
+        return "admin/category";
+    }
+    
     
     
 }
