29bb6abb8c7f4b6bf4e78439da2a2bd663108dd6
Vishal1183-bca <147507684+Vishal1183-bca@users.noreply.github.com>
2025-09-07 13:09:18 +0530

    implement BackEnd

---
+++ src/main/java/com/ecom/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.ecom.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.Product;
+
+public interface ProductService 
+{
+    public Product saveProduct(Product product);
+    public List<Product> getAllProducts();
+    public Boolean deletProduct(Integer id);
+    public Product getProductById(Integer id);
+    public Product updateProduct(Product product,MultipartFile file);
+    
+}
