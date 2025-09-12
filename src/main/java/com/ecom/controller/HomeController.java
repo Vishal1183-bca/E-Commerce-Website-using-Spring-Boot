22c7858eb15f4bb54ea13764ff44f54ef9399945
Vishal1183-bca <147507684+Vishal1183-bca@users.noreply.github.com>
2025-09-12 20:35:09 +0530

    Complete Login Security Feature

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -47,7 +47,7 @@ public class HomeController {
 		return "index";
 	}
 
-	@GetMapping("/login")
+	@GetMapping("/signin")
 	public String login() {
 		return "login";
 	}
--- src/main/java/com/ecom/service/impl/UserServiceImpl.java
+++ src/main/java/com/ecom/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecom.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ecom.model.UserDtls;
@@ -12,10 +14,13 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public UserDtls saveUser(UserDtls userDtls ) {
-        
-
+        userDtls.setRole("ROLE_USER");
+        String encodePassword = passwordEncoder.encode(userDtls.getPassword());
+        userDtls.setPassword(encodePassword);
         UserDtls user = userRepo.save(userDtls);
         return user;
     }
